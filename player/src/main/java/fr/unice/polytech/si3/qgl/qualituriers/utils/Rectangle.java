264b698c632540d7529bec9efaa41c6b7f9e0516
Yann CLODONG <yann.clodong@gmail.com>
2021-01-21 13:20:48 +0100

    ADD: corners Rectangle

--- player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/utils/Rectangle.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/utils/Rectangle.java
@@ -1,5 +1,9 @@
 package fr.unice.polytech.si3.qgl.qualituriers.utils;
 
+/**
+ * @author William D'Andrea
+ * @author Yann Clodong
+ */
 public class Rectangle {
 
     private double width;
@@ -10,5 +14,57 @@ public class Rectangle {
         this.width = width;
         this.height = height;
         this.orientation = orientation;
+    }
+
+    /**
+     * Coin en haut à droite du rectangle
+     * @return Position du coin
+     */
+    public Point upRight() {
+        Position pos = new Position(0, 0, orientation);
+        return  pos.getPoint()                          // position
+                    .add(                               // +
+                pos.up().scalar(height / 2)             // up * h / 2
+                    .add(                               // +
+                pos.right().scalar(width / 2)  ));      // right * w / 2
+    }
+
+    /**
+     * Coin en haut à gauche du rectangle
+     * @return Position du coin
+     */
+    public Point upLeft() {
+        Position pos = new Position(0, 0, orientation);
+        return  pos.getPoint()                          // position
+                    .add(                               // +
+                pos.up().scalar(height / 2)             // up * h / 2
+                    .add(                               // +
+                pos.left().scalar(width / 2)  ));      // left * w / 2
+    }
+
+    /**
+     * Coin en bas à droite du rectangle
+     * @return Position du coin
+     */
+    public Point DownRight() {
+        Position pos = new Position(0, 0, orientation);
+        return  pos.getPoint()                          // position
+                    .add(                               // +
+                pos.down().scalar(height / 2)             // down * h / 2
+                    .add(                               // +
+                pos.right().scalar(width / 2)  ));      // right * w / 2
+    }
+
+    /**
+     * Coin en bas à gauche du rectangle
+     * @return Position du coin
+     */
+    public Point downLeft() {
+        Position pos = new Position(0, 0, orientation);
+        return  pos.getPoint()                          // position
+                    .add(                               // +
+                pos.down().scalar(height / 2)             // down * h / 2
+                    .add(                               // +
+                pos.left().scalar(width / 2)  ));      // left * w / 2
     }
 }
