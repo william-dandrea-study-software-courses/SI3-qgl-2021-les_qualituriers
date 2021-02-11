d3efcb744fde742bd35b531bd0cc14ac1a71fc56
Yann CLODONG <yann.clodong@gmail.com>
2021-02-11 15:49:59 +0100

    ADD: Captain, Foreman

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/entity/boat/boatutils/Captain.java
@@ -0,0 +1,49 @@
+package fr.unice.polytech.si3.qgl.qualituriers.entity.boat.boatutils;
+
+import fr.unice.polytech.si3.qgl.qualituriers.entity.boat.Boat;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.Point;
+
+/**
+ * @author CLODONG Yann
+ */
+public class Captain {
+    private final Boat boat;
+    private Point target;
+
+    public Captain(Boat boat) {
+        this.boat = boat;
+    }
+
+    public void decide() {
+        if(target == null) return;
+        setBend();
+        setSpeed();
+    }
+
+    private void setSpeed() {
+        double dist = boat.getTransform().getPoint().substract(target).length();
+        double distRal = 3;
+        if(dist > distRal) dist = distRal;
+
+        boat.getForeman().setSpeed(dist / distRal);
+    }
+
+    private void setBend() {
+        double angle = boat.getTransform().getAngleToSee(target);
+        double bend = 0;
+
+        if(Math.abs(angle) > Math.PI / 2) {
+            bend = 1;
+        } else if(Math.abs(angle) > Math.PI / 4) {
+            bend = 0.5f;
+        }
+        boolean right = angle < 0;
+
+        if(right) boat.getForeman().setBendRight(bend);
+        else boat.getForeman().setBendLeft(bend);
+    }
+
+    public void GoTo(Point position) {
+        this.target = position;
+    }
+}
---
+++ player/src/test/java/fr/unice/polytech/si3/qgl/qualituriers/entity/boat/boatutils/CaptainTest.java
@@ -0,0 +1,61 @@
+package fr.unice.polytech.si3.qgl.qualituriers.entity.boat.boatutils;
+
+import fr.unice.polytech.si3.qgl.qualituriers.Deck;
+import fr.unice.polytech.si3.qgl.qualituriers.entity.boat.Boat;
+import fr.unice.polytech.si3.qgl.qualituriers.entity.boat.BoatEntity;
+import fr.unice.polytech.si3.qgl.qualituriers.entity.boat.Marin;
+import fr.unice.polytech.si3.qgl.qualituriers.entity.boat.OarBoatEntity;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.Point;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.Transform;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.shape.Shape;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.shape.Shapes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CaptainTest {
+    Boat boat;
+    OarBoatEntity oar;
+
+    void createBoat() {
+        oar = new OarBoatEntity(0,0) {};
+        BoatEntity oar2 = new OarBoatEntity(1,0) {};
+        BoatEntity oar3 = new OarBoatEntity(0,1) {};
+        BoatEntity oar4 = new OarBoatEntity(1,1) {};
+        BoatEntity oar5 = new OarBoatEntity(3,0) {};
+        BoatEntity oar6 = new OarBoatEntity(3,1) {};
+
+
+        Marin sailor1 = new Marin(1,0,1,"sailor1");
+        Marin sailor2 = new Marin(2,1,0,"sailor2");
+        Marin sailor3 = new Marin(3,0,1,"sailor3");
+        Marin sailor4 = new Marin(4,1,1,"sailor4");
+
+        int life = 100;
+        Transform transform = new Transform(0,0,0);
+        String name = "boatTest1";
+        Deck deck = new Deck(2,4);
+        BoatEntity[] entities = {oar6,oar5,oar3,oar4, oar2, oar};
+        Shape shape = new Shape(Shapes.POLYGON) {
+            @Override
+            public boolean isIn(Point position) {
+                return false;
+            }
+        };
+        Marin[] sailors = {sailor4, sailor3,sailor2,sailor1};
+
+
+        boat = new Boat(life, transform, name, deck, entities,shape);
+        boat.setSailors(List.of(sailors));
+    }
+    @BeforeEach
+    void init() {
+        createBoat();
+    }
+
+    @Test
+    void GoToTest() {
+
+    }
+}
