adfe7b21d04d7e947e4ba3e6b6c9645e0a4aeffe
Yann CLODONG <yann.clodong@gmail.com>
2021-02-11 15:51:13 +0100

    ADAPT

--- player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/entity/boat/Marin.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/entity/boat/Marin.java
@@ -1,6 +1,13 @@
 package fr.unice.polytech.si3.qgl.qualituriers.entity.boat;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.Point;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.action.Action;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.action.Moving;
+import fr.unice.polytech.si3.qgl.qualituriers.utils.action.Oar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Cette classe represente un marin qui pourra donner des ordres a différents protagonistes present sur le bateau
@@ -15,11 +22,18 @@ public class Marin {
     private int y;
     private final String name;
 
+    private BoatEntity assignment;
+    private boolean paused = false;
+
     public Marin(@JsonProperty("id")int id, @JsonProperty("x")int x, @JsonProperty("y")int y, @JsonProperty("name")String name) {
         this.id = id;
         this.x = x;
         this.y = y;
         this.name = name;
+    }
+
+    public Point getPosition() {
+        return new Point(x, y);
     }
 
     public int getId() {
@@ -62,5 +76,61 @@ public class Marin {
         if(!(obj instanceof Marin)) return false;
         var castedObj = (Marin)obj;
         return castedObj.id == id;
+    }
+
+    public void assignTo(BoatEntity entity) {
+        this.assignment = entity;
+    }
+
+    public void unAssign() {
+        this.assignment = null;
+    }
+
+    boolean onAssignment() {
+        if(assignment == null) return true;
+        return x == assignment.x && y == assignment.y;
+    }
+
+    private Moving getMovingAction() {
+        Point dest = assignment.getPosition();
+        Point pos = getPosition();
+
+        Point dist = Moving.clamp(dest.substract(pos));
+
+        return new Moving(id, (int)dist.getX(), (int)dist.getY());
+    }
+
+    private Action getAssignmentAction() {
+        if(assignment == null) return null;
+        switch(assignment.type) {
+            case OAR: return new Oar(id);
+        }
+        return null;
+    }
+
+    public List<Action> actionDoneDuringTurn() {
+        List<Action> actions = new ArrayList<>();
+
+        // Go on assignment
+        if(!onAssignment()) {
+            actions.add(getMovingAction());
+        }
+
+        // If I arrived or if I am on assignment
+        if(!paused && Moving.canMove(getPosition(), assignment.getPosition())) {
+            var act = getAssignmentAction();
+            if(act != null)
+                actions.add(act);
+        }
+
+        return actions;
+    }
+
+    public void pause() {
+        this.paused = true;
+    }
+
+    public void resume() {
+        this.paused = false;
     }
 }
