49dfd01e1707b8fa30fdbc6eae80e6f750a40679
Yann CLODONG <yann.clodong@gmail.com>
2021-03-11 20:41:59 +0100

    Add ZERO Constant

--- player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/utils/Transform.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/qualituriers/utils/Transform.java
@@ -14,6 +14,7 @@ import java.util.Objects;
  */
 
 public class Transform extends Point {
+    public static final Transform ZERO = new Transform(new Point(0, 0), 0);
 
     private final double orientation;
 
